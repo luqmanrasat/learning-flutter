bef2c379be261cd84c52bc48457a2e3b8df137e7
luqmanrasat <luqmanrasat@hotmail.com>
2021-10-16 15:44:46 +0800

    add recipes

---
+++ recipes/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+
+    return MaterialApp(
+      title: 'Recipe Calculator',
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+        primary: Colors.grey,
+        secondary: Colors.black,
+      )),
+      home: const MyHomePage(title: 'Recipe Calculator'),
+    );
+  }
+}
