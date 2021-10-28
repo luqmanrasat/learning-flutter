4088c6a23a9802b1618f7d7af524f58f9e91ac37
luqmanrasat <luqmanrasat@hotmail.com>
2021-10-28 13:32:36 +0800

    create grocery item screen

---
+++ fooderlich/lib/screens/grocery_item_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+
+import '../models/models.dart';
+
+class GroceryItemScreen extends StatefulWidget {
+  final Function(GroceryManager) onCreate;
+  final Function(GroceryManager) onUpdate;
+  final GroceryItem? originalItem;
+  final bool isUpdating;
+
+  const GroceryItemScreen({
+    Key? key,
+    required this.onCreate,
+    required this.onUpdate,
+    this.originalItem,
+  })  : isUpdating = (originalItem != null),
+        super(key: key);
+
+  @override
+  _GroceryItemScreenState createState() => _GroceryItemScreenState();
+}
+
+class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  // TODO: Add grocery item screen state properties
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Add GroceryItemScreen Scaffold
+    return Container(color: Colors.orange);
+  }
+
+  // TODO: Add buildNameField()
+
+  // TODO: Add buildImportanceField()
+
+  // TODO: ADD buildDateField()
+
+  // TODO: Add buildTimeField()
+
+  // TODO: Add buildColorPicker()
+
+  // TODO: Add buildQuantityField()
+}
