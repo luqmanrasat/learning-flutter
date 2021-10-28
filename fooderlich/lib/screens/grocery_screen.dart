1ccf1d7bc67c0570178c172edbd9ca0cd6ac9b84
luqmanrasat <luqmanrasat@hotmail.com>
2021-10-28 12:20:11 +0800

    add grocery manager consumer & floating button to grocery screen

--- fooderlich/lib/screens/grocery_screen.dart
+++ fooderlich/lib/screens/grocery_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'empty_grocery_screen.dart';
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add a scaffold widget
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO: Present GrocerItemScreen
+        },
+      ),
+      body: buildGroceryScreen(),
+    );
   }
-  // TODO: Add buildGroceryScreen
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(
+      builder: (context, manager, child) {
+        if (manager.groceryItems.isNotEmpty) {
+          // TODO: Add GroceryListScreen
+          return Container();
+        } else {
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
+  }
 }
