5d2af267b3de5d294d817b9dd2cff4881fd44131
luqmanrasat <luqmanrasat@hotmail.com>
2021-10-31 02:26:16 +0800

    add grocery list

---
+++ fooderlich/lib/screens/grocery_list_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../components/grocery_tile.dart';
+import '../models/models.dart';
+import 'grocery_item_screen.dart';
+
+class GroceryListScreen extends StatelessWidget {
+  final GroceryManager manager;
+
+  const GroceryListScreen({
+    Key? key,
+    required this.manager,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final groceryItems = manager.groceryItems;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView.separated(
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) {
+          final item = groceryItems[index];
+          // TODO: Wrap in a Dismissable
+          // TODO: Wrap in an InkWell
+
+          return GroceryTile(
+            key: Key(item.id),
+            item: item,
+            onComplete: (change) {
+              if (change != null) {
+                manager.completeItem(index, change);
+              }
+            },
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 16.0);
+        },
+      ),
+    );
+  }
+}
--- fooderlich/lib/screens/grocery_screen.dart
+++ fooderlich/lib/screens/grocery_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'empty_grocery_screen.dart';
 import 'grocery_item_screen.dart';
+import 'grocery_list_screen.dart';
 import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
@@ -38,8 +39,7 @@ class GroceryScreen extends StatelessWidget {
     return Consumer<GroceryManager>(
       builder: (context, manager, child) {
         if (manager.groceryItems.isNotEmpty) {
-          // TODO: Add GroceryListScreen
-          return Container();
+          return GroceryListScreen(manager: manager);
         } else {
           return const EmptyGroceryScreen();
         }
