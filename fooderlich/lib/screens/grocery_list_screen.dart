ac9b57261d2ef73460e9cf608af738bd396f2c83
luqmanrasat <luqmanrasat@hotmail.com>
2021-10-31 02:45:34 +0800

    add gesture to delete grocery item

--- fooderlich/lib/screens/grocery_list_screen.dart
+++ fooderlich/lib/screens/grocery_list_screen.dart
@@ -24,31 +24,49 @@ class GroceryListScreen extends StatelessWidget {
           final item = groceryItems[index];
 
           // TODO: Wrap in a Dismissable
-          return InkWell(
-            child: GroceryTile(
-              key: Key(item.id),
-              item: item,
-              onComplete: (change) {
-                if (change != null) {
-                  manager.completeItem(index, change);
-                }
+          return Dismissible(
+            key: Key(item.id),
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              child: const Icon(
+                Icons.delete_forever,
+                color: Colors.white,
+                size: 50.0,
+              ),
+            ),
+            onDismissed: (direction) {
+              manager.deleteItem(index);
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('${item.name} dismissed')));
+            },
+            child: InkWell(
+              child: GroceryTile(
+                key: Key(item.id),
+                item: item,
+                onComplete: (change) {
+                  if (change != null) {
+                    manager.completeItem(index, change);
+                  }
+                },
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GroceryItemScreen(
+                      originalItem: item,
+                      onUpdate: (item) {
+                        manager.updateItem(item, index);
+                        Navigator.pop(context);
+                      },
+                      onCreate: (item) {},
+                    ),
+                  ),
+                );
               },
             ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => GroceryItemScreen(
-                    originalItem: item,
-                    onUpdate: (item) {
-                      manager.updateItem(item, index);
-                      Navigator.pop(context);
-                    },
-                    onCreate: (item) {},
-                  ),
-                ),
-              );
-            },
           );
         },
         separatorBuilder: (context, index) {
