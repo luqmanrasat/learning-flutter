35cdcd08498c5f41ef7c09251a49768d6145525a
luqmanrasat <luqmanrasat@hotmail.com>
2021-11-05 14:03:55 +0800

    handle skip & back button

--- fooderlich/lib/navigation/app_router.dart
+++ fooderlich/lib/navigation/app_router.dart
@@ -56,7 +56,9 @@ class AppRouter extends RouterDelegate
       return false;
     }
 
-    // TODO Handle Onboarding and splash
+    if (route.settings.name == FooderlichPages.onboardingPath) {
+      appStateManager.logout();
+    }
     // TODO Handle state when user closes grocery item screen
     // TODO Handle state when user closes profile screen
     // TODO Handle state when user closes WebView screen
